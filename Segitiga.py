63ba8b433f883ee36ffaec6c3c4155260f83562b
Dermawan-25 <dermawanmawan444@gmail.com>
2025-11-26 11:11:56 +0800

    Create Segitiga.py

---
+++ Segitiga.py
@@ -0,0 +1,10 @@
+# Nama : Dermawan 
+# Nim : D0425319
+# Luas segitiga
+
+L = int(input("masukkan Alas segitiga: "))
+T = int(input("Masukkan tinggi segitiga: "))
+
+luas = int((L * T ) / 2)
+
+print('luas segitiga adalah: ' , luas)
